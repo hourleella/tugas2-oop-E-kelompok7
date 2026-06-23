968f3338672ac9176210891ed40d499382d5a925
hourleella <ellainesalsabila3177@gmail.com>
2026-06-23 17:08:46 +0800

    Membuat subclass Concert.java

---
+++ src/model/Concert.java
@@ -0,0 +1,38 @@
+package model;
+
+public class Concert extends Event implements Refundable {
+    public Concert(String id, String name, String venueId, String organizerId, String date, double basePrice) {
+        super(id, "concert", name, venueId, organizerId, date, basePrice);
+    }
+
+    @Override
+    public double calculateTicketPrice(String category) {
+        switch (category.toLowerCase()) {
+            case "vip":
+                return getBasePrice() * 3.0;
+            case "regular":
+                return getBasePrice() * 1.0;
+            case "festival":
+                return getBasePrice() * 0.7;
+            default:
+                return 0.0;
+        }
+    }
+
+    @Override
+    public double calculateRefund(int daysBeforeEvent) {
+        if (daysBeforeEvent > 14) {
+            return 1.0;
+        } else if (daysBeforeEvent >= 7) {
+            return 0.5;
+        } else {
+            return 0;
+        }
+    }
+
+    @Override
+    public boolean isRefundable() {
+        return true;
+    }
+
+}
